6b1ceb5ff41dfbb3f69ca700b3e1eccde73c50bb
mpryor <mapryor1991@yahoo.com>
2023-06-18 19:45:42 -0600

    Added an exhaustive case (using never) to make sure all unions of type `Easing` are handled

--- packages/draw/src/animation.ts
+++ packages/draw/src/animation.ts
@@ -1,6 +1,5 @@
 type HexColor = string;
 type AnimateableProperty = number | HexColor;
-
 type AnimationState<T extends AnimateableProperty> = {
   initial: T;
   current: T;
@@ -68,10 +67,6 @@ export class Animation<T extends AnimateableProperty> {
     this.startTime = new Date().getTime();
   }
 
-  private numericLerp(start: number, finish: number, percentage: number) {
-    return start + (finish - start) * percentage;
-  }
-
   public getValue(): T {
     const currTime = new Date().getTime();
     const elapsedTime = currTime / 1000 - this.startTime / 1000;
@@ -90,6 +85,10 @@ export class Animation<T extends AnimateableProperty> {
       case 'easeout':
         easingFunc = easeOut;
         break;
+      default:
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars, no-case-declarations
+        const __exhaustiveCase: never = this.easing;
+        throw new Error('Not all easing functions are handled');
     }
 
     const animationPercentage = easingFunc(
@@ -113,9 +112,11 @@ export class Animation<T extends AnimateableProperty> {
     }
     return 0 as T;
   }
-
   public state: AnimationState<T>;
 
+  private numericLerp(start: number, finish: number, percentage: number) {
+    return start + (finish - start) * percentage;
+  }
   private duration: number;
   private easing: Easing;
   private startTime: number;
--- packages/draw/src/style.ts
+++ packages/draw/src/style.ts
@@ -44,22 +44,6 @@ export function createStyles(
   };
 }
 
-export function generateUniqueColors(colorCount: number) {
-  // https://stackoverflow.com/questions/15804149/rgb-color-permutation/15804183#15804183
-  return [...Array(colorCount).keys()].map((i) =>
-    convertRgbArrayToStyle([
-      (i + 1) & 0xff,
-      ((i + 1) & 0xff00) >> 8,
-      ((i + 1) & 0xff0000) >> 16,
-    ]),
-  );
-}
-
-export function convertRgbArrayToStyle(rgbArray: number[]) {
-  if (rgbArray[3] && rgbArray[3] !== 255) return '';
-  return `rgb(${rgbArray.slice(0, 3).join(',')})`;
-}
-
 export function isSSR(): boolean {
   return typeof window === 'undefined';
 }
