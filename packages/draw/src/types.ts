0a39eea59af38195b3f9c353bb2d3a44655914ba
Jeremiah Tabb <jollyjerr@gmail.com>
2023-06-25 10:59:36 -0600

    time to restart lol

--- packages/draw/src/types.ts
+++ packages/draw/src/types.ts
@@ -1,18 +1,6 @@
 import type { Node } from '@mindgraph/types';
 import { SimulationNodeDatum } from 'd3-force';
 
-export interface MindGraphConfig {
-  style?: Partial<GraphStyleConfig>;
-  canvas: HTMLCanvasElement;
-}
-
-export type Color = {
-  r: number;
-  g: number;
-  b: number;
-  a?: number;
-};
-
 export type Circle = {
   x?: number;
   y?: number;
@@ -32,22 +20,19 @@ export type Line = {
 
 export type SimulationNode = SimulationNodeDatum & Node;
 
-export type SimulationLink = SimulationNodeDatum & {
-  source: SimulationNode;
-  target: SimulationNode;
-};
+export type MindGraphEvent = 'nodeClick';
 
 export type NodeClickEvent = { layerX: number; layerY: number };
 
-export type NodeClickCallback = (node: SimulationNode) => void;
-
 export type GraphStyleConfig = {
-  nodeColor: Color;
-  activeNodeColor: Color;
+  nodeColor: string;
+  activeNodeColor: string;
+  dimmedNodeColor: string;
   hoverAnimationDuration: number;
-  linkColor: Color;
-  activeLinkColor: Color;
-  titleColor: Color;
+  linkColor: string;
+  activeLinkColor: string;
+  dimmedLinkColor: string;
+  titleColor: string;
   nodeTitlePadding: number;
   activeNodeTitlePadding: number;
   activeNodeRadiusPadding: number;
@@ -66,3 +51,8 @@ export type GraphSimulationConfig = {
   initialClusterStrength: number;
   randomizeStartingPoints?: boolean;
 };
+
+export interface MindGraphConfig {
+  style?: Partial<GraphStyleConfig>;
+  canvas: HTMLCanvasElement;
+}
