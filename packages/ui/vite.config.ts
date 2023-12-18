d04e160ef30ef94c5c8e56b04fb9ca8fd874035f
Jeremiah Tabb <51095001+jollyjerr@users.noreply.github.com>
2023-12-17 20:03:59 -0700

    refactor: fix exports (#50)

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,11 +1,14 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [dts({ outDir: 'dist', rollupTypes: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'notree',
+      formats: ['es'],
     },
   },
   test: {
